e0d7407bf59af12287660e0952fdeb53672453dc
Andrey Solovyov <slendernet2@gmail.com>
2024-07-17 23:56:55 +0300

    implemented store hooks

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jwtReduser from "./jwt/jwt.slice";
 
-export default configureStore({
+export const store = () => {return configureStore({
     reducer: {
       jwt: jwtReduser
     }
   });
+}
+
+  export type AppStore = ReturnType<typeof store>
+  export type RootState = ReturnType<AppStore['getState']>
+  export type AppDispatch = AppStore['dispatch']
